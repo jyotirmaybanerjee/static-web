74de6a51e40399fcfe46f225e85a0305c3ec1166
Jyotirmay Banerjee <jyotirmay.banerjee@walmart.com>
2021-08-31 08:23:17 -0700

    Navigation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Root } from 'react-static';
 
 import { Contact, Download, Features, Footer, Header, Nav } from './components';
-// import './app.css';
 
 function App() {
   return (
